6ee12a0b6ef68d48813631ba0e32d5241258736c
Rijumone Choudhuri <mailmeonriju@gmail.com>
2024-03-02 10:42:37 +0530

    sources endpoints sans specs and discover

--- app/routers/sources.py
+++ app/routers/sources.py
@@ -1,6 +1,17 @@
-from fastapi import APIRouter, Depends, HTTPException
+from uuid import uuid4
+from fastapi import (APIRouter,
+                    #  Depends,
+                     HTTPException)
+from pydantic import BaseModel
 
+from dat_core.pydantic_models.connector_specification import ConnectorSpecification
 # from ..dependencies import get_token_header
+
+
+class SourceInstance(BaseModel):
+    uuid: str
+    connector_specification: ConnectorSpecification
+
 
 router = APIRouter(
     prefix="/sources",
@@ -10,57 +21,55 @@ router = APIRouter(
 )
 
 
-fake_sources_db = {"plumbus": {
-    "name": "Plumbus"}, "gun": {"name": "Portal Gun"}}
+sources_db = [{"name": "Wikipedia", "connectionSpecification": {}}]
+source_instances_db = []
 
 
-@router.get("/")
-async def read_sources():
-    return fake_sources_db
+@router.get("/available")
+async def fetch_available_sources() -> list[ConnectorSpecification]:
+    return sources_db
 
 
-@router.get("/{source_id}")
-async def read_source(source_id: int):
+@router.get("/{source_slug}")
+async def read_available_source(source_slug: str) -> ConnectorSpecification:
     """Will be populated from manifest file in verified-sources repo"""
-    if source_id not in fake_sources_db:
-        raise HTTPException(status_code=404, detail="source not found")
-    return {"name": fake_sources_db[source_id]["name"], "source_id": source_id}
+    return sources_db[0]
 
 
 @router.post(
-    "/{source_id}",
+    "/{source_slug}/create-instance",
     responses={403: {"description": "Operation forbidden"}},
 )
-async def create_source(source_id: int):
-    if source_id != "plumbus":
-        raise HTTPException(
-            status_code=403, detail="You can only update the source: plumbus"
-        )
-    return {"source_id": source_id, "name": "The great Plumbus"}
+async def create_source_instance(source_slug: str, conn_spec: ConnectorSpecification) -> list[SourceInstance]:
+    source_instances_db.append(SourceInstance(
+        uuid=str(uuid4()), connector_specification=conn_spec))
+    return source_instances_db
 
 
 @router.put(
-    "/{source_id}",
-    # tags=["custom"],
+    "/{source_slug}/update-instance/{source_instance_uuid}",
     responses={403: {"description": "Operation forbidden"}},
 )
-async def update_source(source_id: int):
-    if source_id != "plumbus":
-        raise HTTPException(
-            status_code=403, detail="You can only update the source: plumbus"
-        )
-    return {"source_id": source_id, "name": "The great Plumbus"}
+async def update_source_instance(source_slug: str, source_instance_uuid:
+                                 str, conn_spec: ConnectorSpecification) -> list[SourceInstance]:
+    for idx in range(len(source_instances_db)):
+        source_instance = source_instances_db[idx]
+        if source_instance_uuid != source_instance.uuid:
+            continue
+        source_instances_db[idx] = SourceInstance(
+            uuid=str(uuid4()), connector_specification=conn_spec)
+    return source_instances_db
 
 
-@router.get("/{source_id}/specs")
-async def get_source_specs(source_id: int):
+@router.get("/{source_slug}/specs")
+async def get_source_specs(source_slug: str):
     """Initialize source obj from verified-sources repo and call spec() on it and return"""
-    if source_id not in fake_sources_db:
+    if source_slug not in sources_db:
         raise HTTPException(status_code=404, detail="source not found")
-    return {"name": fake_sources_db[source_id]["name"], "source_id": source_id}
+    return {"name": sources_db[source_slug]["name"], "source_id": source_slug}
 
 
-@router.get("/{source_id}/discover")
-async def call_source_discover(source_id: int):
+@router.get("/{source_slug}/discover")
+async def call_source_discover(source_slug: str):
     """Initialize source obj from verified-sources repo and call discover() on it and return"""
-    return {"name": fake_sources_db[source_id]["name"], "source_id": source_id}
+    return {"name": sources_db[source_slug]["name"], "source_id": source_slug}
