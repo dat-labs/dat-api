27fb4d465d790edc550374bd60ea2fcb5c9c9d56
Rijumone Choudhuri <rijumone.choudhuri@decision-tree.com>
2024-08-20 00:20:43 +0530

    Removed GITBOOK_ACCESS_TOKEN and added value for GITBOOK_SPACE_ID

--- app/config.py
+++ app/config.py
@@ -3,5 +3,4 @@ import os
 
 load_dotenv()  # Load environment variables from .env file
 
-GITBOOK_ACCESS_TOKEN = os.getenv("GITBOOK_ACCESS_TOKEN")
-GITBOOK_SPACE_ID = os.getenv("GITBOOK_SPACE_ID")
+GITBOOK_SPACE_ID = 'Zl7hwJSoQvMvp7WzqUzV'
